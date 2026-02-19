caa1f051a736a1f17228e65db586877048ed1912
Oleg <n7o.nto@yandex.ru>
2026-02-19 18:45:35 +0300

    Added extend function

--- vishnevskiy.oleg/D1/main.cpp
+++ vishnevskiy.oleg/D1/main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
 
+void extend(char *seq, char sym, size_t amount, size_t s)
+{
+  char *tmpseq = new char[s+amount];
+  for (size_t i = 0; i < s; i++)
+  {
+    tmpseq[i] = seq[i];
+  }
+  for (size_t i = s; i < s + amount; i++)
+  {
+    tmpseq[i] = sym;
+  }
+  char *t = seq;
+  seq = tmpseq;
+  tmpseq = t;
+  delete[] t;
+}
+
 int main()
 {
   return 0;
