b900c77f64481253b0cfc0531ec9cbd24da054e9
Daniel <d.m-2006mail.ru@MacBook-Air-Daniel.local>
2026-02-19 19:50:41 +0300

    pull in git

--- makarov.danila/D1/main.cpp
+++ makarov.danila/D1/main.cpp
@@ -6,7 +6,9 @@ int main() {
   unsigned int size = 0;
   unsigned int num = 0;
   char bukv;
-  char h = char new[10];
+  char* h = char new[10];
+  unsigned int cap = 10;
+  int index = 0;
   while (true) {
     std::cin >> num;
     std::cin >> bukv;
@@ -15,18 +17,33 @@ int main() {
       std::cerr << "Invalid input" << '\n';
       return 1;
     }
-    char h = char new[size]
+    if (cap < size) {
+      h = extand(h, cap);
+    }
+    for (int i = 0; i < num; i++) {
+      h[index] = bukv;
+      index++;
+    }
   }
-  return 0;
+  int index2 = size;
+  for (int i = 0; i < size; i++){
+    std::cout << h[index2];
+    index2--;
+  }
 }
 
-void extand(int cap) {
-  int nCap = cap * 2; 
-  int* newData = new int[nCap];
-  for (int i = 0; i < size; i++) {
+char* extand(char *data, unsigned int cap) {
+  unsigned int nCap = cap * 2; 
+  try {
+  char* newData = new char[nCap];
+  for (int i = 0; i < Cap; i++) {
     newData[i] = data[i];
   }
   delete[] data;
-  data = newData;
-  capacity = newCapacity;
+  return newData;
+   } catch(...) {
+    delete [] data;
+    std::cerr << "нету места для массива" << '\n';
+    return 2;
+  }
 }
