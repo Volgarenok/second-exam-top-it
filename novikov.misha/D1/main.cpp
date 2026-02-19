d7c277f649841a8ae6adc17f5856c395c353dcd8
Mishaster82 <misha.novikov2007@gmail.com>
2026-02-19 18:38:29 +0300

    added extend :q

--- novikov.misha/D1/main.cpp
+++ novikov.misha/D1/main.cpp
@@ -1,3 +1,20 @@
 #include <iostream>
+
+namespace novikov {
+  void extend(char ** seq, size_t capacity, size_t newCapacity);
+}
+
 int main()
-{}
+{
+  return 0;
+}
+
+void novikov::extend(char ** seq, size_t capacity, size_t newCapacity)
+{
+  char * newSeq = new char[newCapacity];
+  for (size_t i = 0; i < capacity; ++i) {
+    newSeq[i] = (*seq)[i];
+  }
+  delete [] (*seq);
+  *seq = newSeq;
+}
