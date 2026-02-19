2e418a5257f321a9f5b1f1f11f2eb4dc1f9bb6b2
L1sa620 <lisevbom@gmail.com>
2026-02-19 20:22:07 +0300

    Argc reverse

--- matveev.borislav/D2/main.cpp
+++ matveev.borislav/D2/main.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
 
-int main()
+int main(int argc, char* argv[])
 {
+  bool reverse = false;
+
+  if (argc == 1)
+  {
+    std::cout << "Popa\n";
+  }
+  else if (argc == 2)
+  {
+    const char* arg = argv[1];
+    if (arg[0] == 'r' &&
+        arg[1] == 'e' &&
+        arg[2] == 'v' &&
+        arg[3] == 'e' &&
+        arg[4] == 'r' &&
+        arg[5] == 's' &&
+        arg[6] == 'e' &&
+        arg[7] == '\0')
+    {
+      std::cout << "Popa 2\n";
+      reverse = true;
+    }
+    else
+    {
+      std::cerr << "invalid arguments\n";
+      return 1;
+    }
+  }
+  else
+  {
+    std::cerr << "invalid arguments\n";
+    return 1;
+  }
 
   return 0;
 }
