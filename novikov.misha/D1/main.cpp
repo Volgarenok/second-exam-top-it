a5b0e9d28d3564083aac1320d97572b73ab82e31
Mishaster82 <misha.novikov2007@gmail.com>
2026-02-19 18:44:36 +0300

    added printReversed

--- novikov.misha/D1/main.cpp
+++ novikov.misha/D1/main.cpp
@@ -3,6 +3,7 @@
 namespace novikov {
   void extend(char ** seq, size_t capacity, size_t newCapacity);
   void destroy(char ** seq);
+  void printReversed(const char * seq, size_t size);
 }
 
 int main()
@@ -24,4 +25,12 @@ void novikov::destroy(char ** seq)
 {
   delete [] (*seq);
   *seq = nullptr;
+}
+
+void novikov::printReversed(const char * seq, size_t size)
+{
+  for (size_t i = 0; i = size; i > 0; --i) {
+    std::cout << seq[i - 1];
+  }
+  std::cout << "\n";
 }
