2f47c15920017d05c15cac05d6446809a37544a4
student <student@desktop-5jmrsk4.ius.local>
2026-02-19 19:17:05 +0300

    impl of resize and fill

--- khalikov.mirat/D1/main.cpp
+++ khalikov.mirat/D1/main.cpp
@@ -1,29 +1,44 @@
 #include <iostream>
 
-char * Resize(char * data, size_t size, size_t capacity)
-    {
-      char * new_data = new char[capacity * 2];
-      for (size_t i = 0; i < size; i++)
-      {
-        new_data[i] = data[i];
-      }
-      delete[] data_;
-      capacity *= 2;
-    }
+void resize(char * data, size_t & size, size_t & capacity)
+{
+  char * new_data = new char[capacity * 2];
+  for (size_t i = 0; i < size; i++)
+  {
+    new_data[i] = data[i];
+  }
+  delete[] data;
+  data = new_data;
+  capacity *= 2;
+  size++;
+}
 
+void fill(char * data, char b, size_t size)
+{
+  for (size_t i = 0; i < size; i++)
+  {
+    data[i] = b;
+  }
+}
 
 
 char * createarray(int a);
 
 int main()
 {
-  int b;
-  char c;
+  size_t b;
+  char a;
+  size_t size;
+  size_t capacity;
   char * res = nullptr;
   while (std::cin >> b && std::cin >> a)
   {
     char * data = createarray(b);
-
+    fill(data, a, b);
+    if (size >= capacity)
+    {
+      resize(res, size, capacity);
+    }
 
   }
 }
