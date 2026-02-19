e015c3e3a5558926ea404987366bda8be000c792
Daniel <d.m-2006mail.ru@MacBook-Air-Daniel.local>
2026-02-19 20:25:22 +0300

    fix problem

--- makarov.danila/D1/main.cpp
+++ makarov.danila/D1/main.cpp
@@ -1,49 +1,56 @@
 #include <iostream>
 
-void extend();
+char* extend(char *data, unsigned int &cap);
 
 int main() {
   unsigned int size = 0;
   unsigned int num = 0;
   char bukv;
-  char* h = char new[10];
+  char* h = new char[10];
   unsigned int cap = 10;
   int index = 0;
   while (true) {
     std::cin >> num;
     std::cin >> bukv;
-    size += num;
-    if (std::cin.fail()) {
+    if (std::cin.fail()) { 
       std::cerr << "Invalid input" << '\n';
+      delete[] h;
       return 1;
     }
+    size += num;
     if (cap < size) {
-      h = extand(h, cap);
+      h = extend(h, cap);
+      if (h == nullptr) {
+        std::cerr << "Ошибка расширения массива" << '\n';
+        return 2;
+      }
     }
     for (int i = 0; i < num; i++) {
       h[index] = bukv;
       index++;
     }
   }
-  int index2 = size;
-  for (int i = 0; i < size; i++){
+  int index2 = size - 1;
+  for (int i = 0; i < size; i++) {
     std::cout << h[index2];
     index2--;
   }
+  std::cout << '\n';
+  delete[] h;
 }
 
-char* extand(char *data, unsigned int cap) {
+char* extend(char *data, unsigned int &cap) {
   unsigned int nCap = cap * 2;
   try {
-  char* newData = new char[nCap];
-  for (int i = 0; i < Cap; i++) {
-    newData[i] = data[i];
-  }
-  delete[] data;
-  return newData;
-   } catch(...) {
+    char* newData = new char[nCap];
+    for (int i = 0; i < cap; i++) { 
+      newData[i] = data[i];
+    }
+    delete[] data;
+    cap = nCap;
+    return newData;
+  } catch(...) {
+    return nullptr;
     delete [] data;
-    std::cerr << "нету места для массива" << '\n';
-    return 2;
   }
 }
