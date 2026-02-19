e76abbc02233f93eb45ae9ecded6ae49409e9b6c
sftuuu <artyr27092007@gmail.com>
2026-02-19 20:33:27 +0300

    D_1

--- karhanin.artyr/D1/D1_main.cpp
+++ karhanin.artyr/D1/D1_main.cpp
@@ -6,13 +6,14 @@ int main()
 {
   std::vector<char> seq;
   unsigned int n;
+
   while (std::cin >> n)
   {
     char c;
     if (!(std::cin >> c))
     {
       std::cerr << "Error: не смогли прочитать элементы" << std::endl;
-      for (int i = (int)seq.size() - 1; i >= 0; --i)
+      for (int i = static_cast<int>(seq.size()) - 1; i >= 0; --i)
         std::cout << seq[i];
       std::cout << std::endl;
       return 1;
@@ -21,23 +22,27 @@ int main()
     {
       for (unsigned int i = 0; i < n; ++i)
         seq.push_back(c);
-    } catch (const std::bad_alloc&)
+    }
+    catch (const std::bad_alloc&)
     {
       std::cerr << "Error: не смогли выделить память" << std::endl;
       std::cout << std::endl;
       return 2;
     }
   }
+
   if (std::cin.fail() && !std::cin.eof())
   {
     std::cerr << "Error: не смогли прочитать число" << std::endl;
-    for (int i = (int)seq.size() - 1; i >= 0; --i)
+    for (int i = static_cast<int>(seq.size()) - 1; i >= 0; --i)
       std::cout << seq[i];
     std::cout << std::endl;
     return 1;
   }
-  for (int i = (int)seq.size() - 1; i >= 0; --i)
+
+  for (int i = static_cast<int>(seq.size()) - 1; i >= 0; --i)
     std::cout << seq[i];
   std::cout << std::endl;
+
   return 0;
 }
