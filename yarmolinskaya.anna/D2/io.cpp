d92cf4b483d35a279e7c6249eb33dc8906590768
prblmtc11 <prblmtc11@gmail.com>
2026-02-19 19:41:27 +0300

    added functions in io.cpp

--- yarmolinskaya.anna/D2/io.cpp
+++ yarmolinskaya.anna/D2/io.cpp
@@ -21,4 +21,37 @@ static void printPair(char ch, std::size_t count)
 {
   std::cout << count << ' ' << ch << '\n';
 }
+
+void yarmolinskaya::printPairs(const sequence_t &seq, bool reverse)
+{
+  if (seq.size == 0) {
+    std::cout << '\n';
+    return;
+  }
+
+  if (!reverse) {
+    std::size_t i = 0;
+    while (i < seq.size) {
+      const char current = seq.data[i];
+      std::size_t count = 0;
+      while (i < seq.size && seq.data[i] == current) {
+        ++count;
+        ++i;
+      }
+      printPair(current, count);
+    }
+  } else {
+    std::size_t i = seq.size;
+    while (i > 0) {
+      const char current = seq.data[i - 1];
+      std::size_t count = 0;
+      while (i > 0 && seq.data[i - 1] == current) {
+        ++count;
+        --i;
+      }
+      printPair(current, count);
+    }
+  }
+}
+
 }
