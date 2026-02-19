c310ed1985d77b6fac0e85f12872806b30b6e559
student <student@desktop-5jmrsk4.ius.local>
2026-02-19 19:41:56 +0300

    impl of right alg

--- khalikov.mirat/D1/main.cpp
+++ khalikov.mirat/D1/main.cpp
@@ -10,7 +10,6 @@ void resize(char * data, size_t & size, size_t & capacity)
   delete[] data;
   data = new_data;
   capacity *= 2;
-  size++;
 }
 
 void fill(char * data, char b, size_t size)
@@ -23,17 +22,15 @@ void fill(char * data, char b, size_t size)
 
 void fillarray(char * res, char * data, size_t & size, size_t b, size_t & capacity)
 {
-  if (b > capacity)
+  if (size + b > capacity)
   {
     resize(res, size, capacity);
   }
-  else
+  for (size_t i = 0; i < b; i++)
   {
-    for (size_t i = 0; i < b; i++)
-    {
-      res[size++] = data[i];
-    }
+    res[size++] = data[i];
   }
+
 }
 
 
@@ -46,15 +43,24 @@ int main()
 {
   size_t b;
   char a;
-  size_t size;
-  size_t capacity;
-  char * res = nullptr;
+  size_t size = 0;
+  size_t capacity = 10;
+  char * res = createarray(capacity);
   char * data = nullptr;
   while (std::cin >> b && std::cin >> a)
   {
-    data = createarray(b);
-    fill(data, a, b);
-    fillarray(res, data, size, b, capacity);
+    try
+    {
+      data = createarray(b);
+      fill(data, a, b);
+      fillarray(res, data, size, b, capacity);
+    }
+    catch (const std::bad_alloc &)
+    {
+      delete[] data;
+      delete[] res;
+      return 2;
+    }
   }
 
   std::cout << res;
