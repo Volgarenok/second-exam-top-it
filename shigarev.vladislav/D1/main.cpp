54ccc07e8fbf7a66b323f5c1a50c833171612e5a
Владислав  Шигарев <vladislavsigarev@MBPWiFi.local>
2026-02-19 19:34:51 +0300

    A bit of cg fixes

--- shigarev.vladislav/D1/main.cpp
+++ shigarev.vladislav/D1/main.cpp
@@ -3,103 +3,99 @@
 #include <new>
 
 namespace shigarev {
-
-struct Pair {
-  unsigned int count;
-  char ch;
+  struct Pair {
+    unsigned int count;
+    char ch;
 };
 
-bool readPair(unsigned int & count, char & ch)
-{
+bool readPair(unsigned int & count, char & ch) {
   return static_cast< bool >(std::cin >> count >> ch);
 }
 
 class PairArray {
-public:
-  PairArray():
-    data_(nullptr),
-    size_(0)
-  {}
+  public:
+    PairArray():
+      data_(nullptr),
+      size_(0)
+    {}
 
-  ~PairArray()
-  {
-    delete[] data_;
-  }
-
-  size_t size() const
-  {
-    return size_;
-  }
-
-  void append(unsigned int count, char ch)
-  {
-    Pair * grown = new Pair[size_ + 1];
-    for (size_t i = 0; i < size_; ++i) {
-      grown[i] = data_[i];
+    ~PairArray()
+    {
+      delete[] data_;
     }
-    grown[size_] = {count, ch};
-    delete[] data_;
-    data_ = grown;
-    ++size_;
-  }
 
-  size_t computeLength() const
-  {
-    size_t total = 0;
-    for (size_t i = 0; i < size_; ++i) {
-      total += data_[i].count;
+    size_t size() const
+    {
+      return size_;
     }
-    return total;
-  }
 
-  const Pair & operator[](size_t i) const
-  {
-    return data_[i];
-  }
+    void append(unsigned int count, char ch)
+    {
+      Pair * grown = new Pair[size_ + 1];
+      for (size_t i = 0; i < size_; ++i) {
+        grown[i] = data_[i];
+      }
+      grown[size_] = {count, ch};
+      delete[] data_;
+      data_ = grown;
+      ++size_;
+    }
 
-private:
-  Pair * data_;
-  size_t size_;
+    size_t computeLength() const {
+      size_t total = 0;
+      for (size_t i = 0; i < size_; ++i) {
+        total += data_[i].count;
+      }
+      return total;
+    }
+
+    const Pair & operator[](size_t i) const
+    {
+      return data_[i];
+    }
+
+  private:
+    Pair * data_;
+    size_t size_;
 };
 
 class CharSequence {
-public:
-  explicit CharSequence(const PairArray & pairs):
-    length_(pairs.computeLength()),
-    data_(new char[length_])
-  {
-    size_t pos = 0;
-    for (size_t i = 0; i < pairs.size(); ++i) {
-      for (unsigned int j = 0; j < pairs[i].count; ++j) {
-        data_[pos] = pairs[i].ch;
-        ++pos;
+  public:
+    explicit CharSequence(const PairArray & pairs):
+      length_(pairs.computeLength()),
+      data_(new char[length_])
+    {
+      size_t pos = 0;
+      for (size_t i = 0; i < pairs.size(); ++i) {
+        for (unsigned int j = 0; j < pairs[i].count; ++j) {
+          data_[pos] = pairs[i].ch;
+          ++pos;
+        }
       }
     }
-  }
 
-  ~CharSequence()
-  {
-    delete[] data_;
-  }
-
-  size_t length() const
-  {
-    return length_;
-  }
-
-  void printReverse() const
-  {
-    for (size_t i = length_; i > 0; --i) {
-      std::cout << data_[i - 1];
+    ~CharSequence()
+    {
+      delete[] data_;
     }
-    std::cout << '\n';
-  }
+  
+    size_t length() const
+    {
+      return length_;
+    }
 
-private:
-  size_t length_;
-  char * data_;
-};
+    void printReverse() const
+    {
+      for (size_t i = length_; i > 0; --i) {
+        std::cout << data_[i - 1];
+      }
+      std::cout << '\n';
+    }
 
+  private:
+    size_t length_;
+    char * data_;
+  };
 }
 
 int main() {
