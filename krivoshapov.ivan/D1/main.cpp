322eab427d9c048d12aa348652ba9d9f7aa12b7f
ahahhaha67 <krivoshapovewan@yandex.ru>
2026-02-19 18:55:53 +0300

    end Iprocessor realization

--- krivoshapov.ivan/D1/main.cpp
+++ krivoshapov.ivan/D1/main.cpp
@@ -13,7 +13,12 @@ namespace krivoshapov
     {
       doPrint();
     }
-  }
+    virtual ~IProcessor() = default;
+
+  protected:
+    virtual void doAddPair(unsigned int num, char ch) = 0;
+    virtual void doPrint();
+  };
 }
 int main()
 {
