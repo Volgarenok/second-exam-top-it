280103ff2d44ddd669b38c77521ae265ddb538e6
uncharted-bread <gordeychikmika@gmail.com>
2026-02-19 18:53:22 +0300

    add freeSequence and appendChars funcs

--- gordejchik.mikhail/D1/main.cpp
+++ gordejchik.mikhail/D1/main.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
+#include <cstddef>
+
+namespace gordejchik
+{
+  void freeSequence(char*& data)
+  {
+    delete[] data;
+    data = nullptr;
+  }
+
+  bool appendChars(char*& data, size_t& size, char ch, size_t count)
+  {
+    if (count == 0) {
+      return true;
+    }
+    char* newData = nullptr;
+    try {
+      newData = new char[size + count];
+    } catch (const std::bad_alloc&) {
+      return false;
+    }
+    for (size_t i = 0; i < size; ++i) {
+      newData[i] = data[i];
+    }
+    for (size_t i = 0; i < count; ++i) {
+      newData[size + i] = ch;
+    }
+    delete[] data;
+    data = newData;
+    size += count;
+    return true;
+  }
+}
 
 int main()
 {
+  char* data = nullptr;
+  size_t size = 0;
+  gordejchik::appendChars(data, size, 'a', 2);
+  gordejchik::freeSequence(data);
   return 0;
 }
