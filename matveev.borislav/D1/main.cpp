d7885a467c900c0fb49e2149f96976657939a0ae
L1sa620 <lisevbom@gmail.com>
2026-02-19 18:56:47 +0300

    Input

--- matveev.borislav/D1/main.cpp
+++ matveev.borislav/D1/main.cpp
@@ -2,5 +2,22 @@
 
 int main()
 {
+  unsigned int b;
+  char a;
+  while (std::cin >> b >> a)
+  {
+    while (b)
+    {
+      std::cout << a;
+      --b;
+    }
+    std::cout << "\n";
+  }
+
+  if (!std::cin.eof())
+  {
+    std::cerr << "Input error\n";
+    return 1;
+  }
   return 0;
 }
