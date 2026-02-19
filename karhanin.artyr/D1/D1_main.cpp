d79d9c024ae803b35a92f4bdb1fa0d36df84cc67
sftuuu <artyr27092007@gmail.com>
2026-02-19 20:27:48 +0300

    D1

---
+++ karhanin.artyr/D1/D1_main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <stdexcept>
+
+int main()
+{
+  std::vector<char> seq;
+  unsigned int n;
+  while (std::cin >> n)
+  {
+    char c;
+    if (!(std::cin >> c))
+    {
+      std::cerr << "Error: не смогли прочитать элементы" << std::endl;
+      for (int i = (int)seq.size() - 1; i >= 0; --i)
+        std::cout << seq[i];
+      std::cout << std::endl;
+      return 1;
+    }
+    try
+    {
+      for (unsigned int i = 0; i < n; ++i)
+        seq.push_back(c);
+    } catch (const std::bad_alloc&)
+    {
+      std::cerr << "Error: не смогли выделить память" << std::endl;
+      std::cout << std::endl;
+      return 2;
+    }
+  }
+  if (std::cin.fail() && !std::cin.eof())
+  {
+    std::cerr << "Error: не смогли прочитать число" << std::endl;
+    for (int i = (int)seq.size() - 1; i >= 0; --i)
+      std::cout << seq[i];
+    std::cout << std::endl;
+    return 1;
+  }
+  for (int i = (int)seq.size() - 1; i >= 0; --i)
+    std::cout << seq[i];
+  std::cout << std::endl;
+  return 0;
+}
