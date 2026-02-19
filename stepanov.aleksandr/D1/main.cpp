7805120f5cd2fbee02dc874f96dce6b4868479d0
btless <zovitemenakrutoj@gmail.com>
2026-02-19 18:37:57 +0300

    implemented reading input

---
+++ stepanov.aleksandr/D1/main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+
+namespace stepanov
+{
+  using repeatedChar = std::pair<char, size_t>;
+  repeatedChar readLine(std::istream& in, bool& success);
+  repeatedChar* readInput(std::istream& in, size_t& size);
+}
+
+int main()
+{
+  size_t size = 0;
+  stepanov::repeatedChar* data = nullptr;
+  try {
+    data = stepanov::readInput(std::cin, size);
+  } catch (const std::bad_alloc& e) {
+    std::cerr << "bad alloc\n";
+    return 1;
+  } catch (const std::runtime_error& e) {
+    std::cerr << e.what() << '\n';
+    return 2;
+  }
+  return 0;
+}
+
+stepanov::repeatedChar stepanov::readLine(std::istream& in, bool& success)
+{
+  repeatedChar line = { '\0', 0 };
+  in >> line.first;
+  if (in.fail()) {
+    success = false;
+    return line;
+  }
+  in >> line.second;
+  if (in.fail() && !in.eof()) {
+    success = false;
+    return line;
+  }
+  return line;
+}
+
+stepanov::repeatedChar* stepanov::readInput(std::istream& in, size_t& size)
+{
+  const size_t step = 10;
+  size_t capacity = 10;
+  auto* mem = new stepanov::repeatedChar[capacity];
+
+  bool success = true;
+  auto c = readLine(in, success);
+  while (!in.eof() && success) {
+    if (size + 1 >= capacity) {
+      stepanov::repeatedChar* new_mem = nullptr;
+      try {
+        new_mem = new stepanov::repeatedChar[capacity + step];
+      } catch (const std::bad_alloc& e) {
+        delete[] mem;
+        throw;
+      }
+      capacity += step;
+      for (size_t i = 0; i < size; i++) {
+        new_mem[i] = mem[i];
+      }
+      delete[] mem;
+      mem = new_mem;
+    }
+    mem[size] = c;
+    size++;
+    c = readLine(in, success);
+  }
+
+  if (!success) {
+    delete[] mem;
+    throw std::runtime_error("readInput: bad input");
+  }
+  return mem;
+}
