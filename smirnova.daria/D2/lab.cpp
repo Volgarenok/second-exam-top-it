c772e7c9089c994b334c007b1b4f903dd0968a80
dassrm <daryasm1rnoff@yandex.ru>
2026-02-19 19:35:51 +0300

    Add character reading and counting logic

--- smirnova.daria/D2/lab.cpp
+++ smirnova.daria/D2/lab.cpp
@@ -1,8 +1,46 @@
 #include "lab.hpp"
 
 #include <cstring>
+#include <iostream>
 
 namespace smirnova {
+
+namespace {
+
+const int MAX_CHARS = 256;
+
+struct CharTable {
+  int counts[MAX_CHARS];
+  char order[MAX_CHARS];
+  int orderSize;
+};
+
+void initTable(CharTable & table)
+{
+  for (int i = 0; i < MAX_CHARS; ++i) {
+    table.counts[i] = 0;
+    table.order[i] = 0;
+  }
+  table.orderSize = 0;
+}
+
+void readInput(CharTable & table)
+{
+  char ch = 0;
+  while (std::cin.get(ch)) {
+    if (ch == ' ' || ch == '\t' || ch == '\n' || ch == '\r') {
+      continue;
+    }
+    const int idx = static_cast<unsigned char>(ch);
+    if (table.counts[idx] == 0) {
+      table.order[table.orderSize] = ch;
+      ++table.orderSize;
+    }
+    ++table.counts[idx];
+  }
+}
+
+} // namespace
 
 bool parseArgs(int argc, char const * const * argv, ParseResult & result)
 {
@@ -19,6 +57,9 @@ bool parseArgs(int argc, char const * const * argv, ParseResult & result)
 
 void processInput(bool reverse)
 {
+  CharTable table;
+  initTable(table);
+  readInput(table);
   (void)reverse;
 }
 
