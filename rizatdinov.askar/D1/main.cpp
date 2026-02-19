3b8e5992c1d7bca7bc0130adac4354708fba9da7
Asgard <rizatdinov06@mail.ru>
2026-02-19 20:33:49 +0300

    The input handler has been finalized and the output has been completed

--- rizatdinov.askar/D1/main.cpp
+++ rizatdinov.askar/D1/main.cpp
@@ -4,7 +4,7 @@ namespace rizatdinov
 {
   void extend(char ** str_array, size_t k, size_t d);
   void extend(unsigned ** num_array, size_t k, size_t d);
-  void handleInput(unsigned ** num_array, char ** str_array, size_t & size);
+  size_t handleInput(unsigned ** num_array, char ** str_array);
 }
 
 int main()
@@ -14,19 +14,28 @@ int main()
   size_t size = 0;
 
   try {
-    rizatdinov::handleInput(&num_array, &str_array, size);
+    size = rizatdinov::handleInput(&num_array, &str_array);
   } catch(...) {
-    std::cout << "Error: 1\n";
+    std::cerr << "Error: 2\n";
 
     delete[] num_array;
     delete[] str_array;
 
+    return 2;
+  }
+
+  if (!size) {
+    std::cerr << "Error: 1\n";
     return 1;
   }
 
-  for (size_t i = 0; i < size; ++i) {
-    std::cout << num_array[i] << ' ' << str_array[i] << '\n';
+  for (size_t i = 1; i <= size; ++i) {
+    unsigned count = num_array[size - i];
+    for (size_t j = 0; j < count; ++j) {
+      std::cout << str_array[size - i];
+    }
   }
+  std::cout << '\n';
 
   delete[] num_array;
   delete[] str_array;
@@ -54,7 +63,7 @@ void rizatdinov::extend(unsigned ** num_array, size_t k, size_t d)
   *num_array = new_array;
 }
 
-void rizatdinov::handleInput(unsigned ** num_array, char ** str_array, size_t & size)
+size_t rizatdinov::handleInput(unsigned ** num_array, char ** str_array)
 {
   size_t sizeD = 2;
   char * str_data = new char[sizeD];
@@ -68,10 +77,18 @@ void rizatdinov::handleInput(unsigned ** num_array, char ** str_array, size_t & 
       sizeD += sizeD*2;
     }
   }
+  if ((std::cin.bad() || std::cin.fail()) && !std::cin.eof()) {
+    delete[] num_data;
+    delete[] str_data;
+
+    return 0;
+  }
+
   rizatdinov::extend(&str_data, sizeD, i);
   rizatdinov::extend(&num_data, sizeD, i);
 
-  size = i;
   *str_array = str_data;
   *num_array = num_data;
+
+  return i;
 }
