e5a2618bf30fb46c4eb50208db30065f02923f39
uncharted-bread <gordeychikmika@gmail.com>
2026-02-19 18:58:25 +0300

    added readSequence and printReversed funcs

--- gordejchik.mikhail/D1/main.cpp
+++ gordejchik.mikhail/D1/main.cpp
@@ -31,13 +31,40 @@ namespace gordejchik
     size += count;
     return true;
   }
+
+  int readSequence(std::istream& in, char*& data, size_t& size)
+  {
+    size_t count = 0;
+    char ch = '\0';
+    while (in >> count) {
+      if (!(in >> ch)) {
+        return 1;
+      }
+      if (!appendChars(data, size, ch, count)) {
+        return 2;
+      }
+    }
+    if (!in.eof()) {
+      return 1;
+    }
+    return 0;
+  }
+
+  void printReversed(std::ostream& out, const char* data, size_t size)
+  {
+    for (size_t i = size; i > 0; --i) {
+      out << data[i - 1];
+    }
+    out << '\n';
+  }
 }
 
 int main()
 {
   char* data = nullptr;
   size_t size = 0;
-  gordejchik::appendChars(data, size, 'a', 2);
+  gordejchik::readSequence(std::cin, data, size);
+  gordejchik::printReversed(std::cout, data, size);
   gordejchik::freeSequence(data);
   return 0;
 }
