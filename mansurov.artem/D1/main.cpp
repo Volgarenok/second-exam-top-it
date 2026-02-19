6d0ed2817f27ff97b355f94f3bd693990e58548b
root <mansurov.artem07@gmail.com>
2026-02-19 20:10:26 +0300

    with main and func

--- mansurov.artem/D1/main.cpp
+++ mansurov.artem/D1/main.cpp
@@ -1,24 +1,95 @@
 #include <iostream>
+#include <typeinfo>
+#include <string>
 namespace mansurov{
-    size_t* extend(size_t** numbers, size_t new_num);
+  void extend(size_t** numbers,size_t count, size_t new_num);
+  void extend(char** chars,size_t count, char new_char);
+  char* in_string(const size_t* numbers,const char* symbols, size_t n, size_t& count);
+  void flip(char** symbols, size_t n);
 }
 int main(){
-    using namespace mansurov;
-    size_t* numbers = nullptr;
-    char* symbols = nullptr;
-    size_t count = 0;
-    try{
-        while(std::cin){
-            size_t tmp_num = 0;
-            std::cin >> tmp_num;
-            if(!std::cin){
-                if(std::cin.eof()){
-                    std::cout << "missing symbol" << "\n";
-                }
-            }
-            char tmp_char = ' ';
-            std::cin >> tmp_char;
-            if()
+  using namespace mansurov;
+  size_t* numbers = nullptr;
+  char* symbols = nullptr;
+  size_t count = 0;
+  try{
+    while(std::cin){
+      size_t tmp_num = 0;
+      std::cin >> tmp_num;
+      if(!std::cin){
+        if(std::cin.eof()){
+          break;
         }
+        return 1;
+      }
+      else{
+        extend(&numbers,count, tmp_num);          
+      }
+      char tmp_char = ' ';
+      std::cin >> tmp_char;
+      if(!std::cin){
+        if(std::cin.eof()){
+          break;
+        }
+        return 1;
+      }
+      else{
+        extend(&symbols,count, tmp_char);
+      }
+      count++;
     }
+    size_t count_chars = 0;
+    char* strs = in_string(numbers, symbols, count, count_chars);
+  }
+  catch(const std::bad_alloc & e){
+    std::cerr << "bad alloc" << e.what() << std::endl;
+    return 1;
+  }
+}
+
+void mansurov::extend(size_t ** num, size_t n, size_t count)
+{
+  size_t * new_num = new size_t[n + 1];
+  for (size_t i = 0; i < n; ++i) {
+    new_num[i] = (*num)[i];
+  }
+  new_num[n] = count;
+  delete [] (*num);
+  *num = new_num;
+}
+
+void mansurov::extend(char ** num, size_t n, char count)
+{
+  char * new_num = new char[n + 1];
+  for (size_t i = 0; i < n; ++i) {
+    new_num[i] = (*num)[i];
+  }
+  new_num[n] = count;
+  delete [] (*num);
+  *num = new_num;
+}
+
+char* mansurov::in_string(const size_t* numbers,const char* symbols, size_t n, size_t& count){
+  for(size_t i =0; i < n; i++){
+    count+=numbers[i];
+  }
+  char* new_str = new char[n*count];
+  size_t c = 0;
+  for(size_t i = 0; i < n; i++){
+    for(size_t k = 0; k < numbers[i]; k++){
+      new_str[c] = symbols[i];
+      c++;
+    }
+  }
+  return new_str;
+}
+
+
+void mansurov::flip(char** symbols, size_t n){
+  char* new_symbols = new char[n];
+  for(size_t i = 0; i < n; i++){
+    new_symbols[n-i-1] = *symbols[i];
+  }
+  delete[] (*symbols);
+  *symbols = new_symbols;
 }
