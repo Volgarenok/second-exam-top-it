ff4eb422cce82d6bc5501e590ceaba4b99d7d50f
L1sa620 <lisevbom@gmail.com>
2026-02-19 19:10:30 +0300

    Reverse output for pairs

--- matveev.borislav/D1/main.cpp
+++ matveev.borislav/D1/main.cpp
@@ -4,20 +4,30 @@ int main()
 {
   unsigned int b;
   char a;
+  std::size_t max = 100000;
+  char data[max];
+  std::size_t sum = 0;
+
   while (std::cin >> b >> a)
   {
-    while (b)
+    for (unsigned int i = 0; i < b; ++i)
     {
-      std::cout << a;
-      --b;
+      if (sum < max)
+      {
+        data[sum++] = a;
+      }
     }
-    std::cout << "\n";
   }
 
   if (!std::cin.eof())
   {
-    std::cerr << "Input error\n";
+    std::cerr << "input error\n";
     return 1;
   }
+  for (std::size_t i = sum; i > 0; --i)
+  {
+    std::cout << data[i-1];
+  }
+  std::cout << '\n';
   return 0;
 }
