70a0e9771c69b903ed3e4d979c7529bd41b570b7
skip <89522640535egmail.com>
2026-02-19 19:39:29 +0300

    solve return 1 added

--- emushintsev.egor/D1/main.cpp
+++ emushintsev.egor/D1/main.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
 #include <new>
 
+namespace emushintsev{
+
 void print(char* arr, size_t s){
     if (arr != nullptr && s > 0){
         for (size_t i = s; i > 0; i--){
             std::cout << arr[i - 1];
+        }
+    }
+    std::cout << std::endl;
+    delete[] arr;
+}
+
+int solve(){
+    size_t s = 0;
+    size_t cap = 0;
+    char* arr = nullptr;
+    unsigned int count;
+    char symb;
+    while (true){
+        std::cin >> count;
+        if(std::cin.fail()){
+            if(std::cin.eof()){
+                break;
+            }
+            print(arr, s);
+            return 1;
         }
     }
 }
