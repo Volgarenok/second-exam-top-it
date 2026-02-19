09870c1f27afdbe7d12e88e977cfc653cd24867b
Михаил Лачугин <mikee@MacBook-Air-Mihail.local>
2026-02-19 18:48:37 +0300

    add cin

--- lachugin.mikhail/D1/main.cpp
+++ lachugin.mikhail/D1/main.cpp
@@ -1,4 +1,20 @@
+#include <iostream>
+
 int main()
 {
+  size_t n = 0;
+  char k = 0;
 
+  char** arrs = nullptr;
+  size_t* longs = nullptr;
+
+  while (std::cin >> n)
+  {
+    char* arr = new char[n];
+    for (size_t i = 0; i < n; ++i)
+    {
+      std::cin >> arr[i];
+    }
+
+  }
 }
