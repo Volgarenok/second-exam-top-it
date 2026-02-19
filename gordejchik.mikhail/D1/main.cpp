27b0ab1317662f965e9674ec947ccc0cea1edb47
uncharted-bread <gordeychikmika@gmail.com>
2026-02-19 19:01:18 +0300

    moved funcs to a struct

--- gordejchik.mikhail/D1/main.cpp
+++ gordejchik.mikhail/D1/main.cpp
@@ -3,68 +3,79 @@
 
 namespace gordejchik
 {
-  void freeSequence(char*& data)
+  struct CharSequence
   {
-    delete[] data;
-    data = nullptr;
-  }
+    char* data;
+    size_t size;
 
-  bool appendChars(char*& data, size_t& size, char ch, size_t count)
-  {
-    if (count == 0) {
+    CharSequence():
+      data(nullptr),
+      size(0)
+    {}
+
+    void free()
+    {
+      delete[] data;
+      data = nullptr;
+      size = 0;
+    }
+
+    bool append(char ch, size_t count)
+    {
+      if (count == 0) {
+        return true;
+      }
+      char* newData = nullptr;
+      try {
+        newData = new char[size + count];
+      } catch (const std::bad_alloc&) {
+        return false;
+      }
+      for (size_t i = 0; i < size; ++i) {
+        newData[i] = data[i];
+      }
+      for (size_t i = 0; i < count; ++i) {
+        newData[size + i] = ch;
+      }
+      delete[] data;
+      data = newData;
+      size += count;
       return true;
     }
-    char* newData = nullptr;
-    try {
-      newData = new char[size + count];
-    } catch (const std::bad_alloc&) {
-      return false;
-    }
-    for (size_t i = 0; i < size; ++i) {
-      newData[i] = data[i];
-    }
-    for (size_t i = 0; i < count; ++i) {
-      newData[size + i] = ch;
-    }
-    delete[] data;
-    data = newData;
-    size += count;
-    return true;
-  }
 
-  int readSequence(std::istream& in, char*& data, size_t& size)
-  {
-    size_t count = 0;
-    char ch = '\0';
-    while (in >> count) {
-      if (!(in >> ch)) {
+    int read(std::istream& in)
+    {
+      size_t count = 0;
+      char ch = '\0';
+      while (in >> count) {
+        if (!(in >> ch)) {
+          return 1;
+        }
+        if (!append(ch, count)) {
+          return 2;
+        }
+      }
+      if (!in.eof()) {
         return 1;
       }
-      if (!appendChars(data, size, ch, count)) {
-        return 2;
-      }
+      return 0;
     }
-    if (!in.eof()) {
-      return 1;
-    }
-    return 0;
-  }
 
-  void printReversed(std::ostream& out, const char* data, size_t size)
-  {
-    for (size_t i = size; i > 0; --i) {
-      out << data[i - 1];
+    void printReversed(std::ostream& out) const
+    {
+      for (size_t i = size; i > 0; --i) {
+        out << data[i - 1];
+      }
+      out << '\n';
     }
-    out << '\n';
-  }
+  };
 }
 
 int main()
 {
-  char* data = nullptr;
-  size_t size = 0;
-  gordejchik::readSequence(std::cin, data, size);
-  gordejchik::printReversed(std::cout, data, size);
-  gordejchik::freeSequence(data);
-  return 0;
+  gordejchik::CharSequence seq;
+  const int result = seq.read(std::cin);
+  seq.printReversed(std::cout);
+  seq.free();
+  return result;
 }
