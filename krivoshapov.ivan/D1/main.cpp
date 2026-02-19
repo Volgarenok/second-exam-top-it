9bafaa24cf9c527aa69c99b70a661f498b7050f8
ahahhaha67 <krivoshapovewan@yandex.ru>
2026-02-19 19:05:02 +0300

    rework

--- krivoshapov.ivan/D1/main.cpp
+++ krivoshapov.ivan/D1/main.cpp
@@ -1,25 +1,5 @@
 #include <iostream>
 
-namespace krivoshapov
-{
-  class IProcessor
-  {
-  public:
-    void addPair(unsigned int num, char ch)
-    {
-      doAddPair(num, ch);
-    }
-    void print()
-    {
-      doPrint();
-    }
-    virtual ~IProcessor() = default;
-
-  protected:
-    virtual void doAddPair(unsigned int num, char ch) = 0;
-    virtual void doPrint();
-  };
-}
 int main()
 {
 }
