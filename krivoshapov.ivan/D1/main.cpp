0d4e275d356d1796c55476c74c64ab2cc7a8af1e
ahahhaha67 <krivoshapovewan@yandex.ru>
2026-02-19 18:53:23 +0300

    IProcessor realization

--- krivoshapov.ivan/D1/main.cpp
+++ krivoshapov.ivan/D1/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
+namespace krivoshapov
+{
+  class IProcessor
+  {
+  public:
+    void addPair(unsigned int num, char ch)
+    {
+      doAddPair(num, ch);
+    }
+    void print()
+    {
+      doPrint();
+    }
+  }
+}
 int main()
 {
-
 }
