ec98a9eeb57951a8487187e168a6c10e4796fef7
Mishaster82 <misha.novikov2007@gmail.com>
2026-02-19 18:55:53 +0300

    added catching errors

--- novikov.misha/D1/main.cpp
+++ novikov.misha/D1/main.cpp
@@ -9,7 +9,19 @@ namespace novikov {
 
 int main()
 {
-  return 0;
+  using namespace novikov;
+  char * seq = nullptr;
+  size_t size = 0;
+  size_t capacity = 0;
+  int result = readInput(&seq, &size, &capacity);
+  printReversed(seq, size);
+  destroy(&seq);
+  if (result == 1) {
+    std::cerr << "read error\n";
+  } else if (result == 2) {
+    std::cerr << "bad alloc\n";
+  }
+  return result;
 }
 
 void novikov::extend(char ** seq, size_t capacity, size_t newCapacity)
@@ -50,7 +62,12 @@ int novikov::readInput(char ** seq, size_t * size, size_t * capacity)
     }
     for (size_t i = 0; i < count; ++i) {
       if (*size == *capacity) {
-        size_t newCapacity = (*capacity == 0) ? 1 : (*capacity) * 2;
+        size_t newCapacity = 0;
+        if (*capacity == 0) {
+          newCapacity = 1;
+        } else {
+          newCapacity = (*capacity) * 2;
+        }
         try {
           extend(seq, *capacity, newCapacity);
         } catch (const std::bad_alloc &) {
