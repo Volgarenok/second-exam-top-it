0113d92d905b5cdddba95cf2685adcc5eed26c71
liia <kirilich_lubit@rambler.ru>
2026-02-19 22:08:36 +0500

    add from D1 struct and func add check error arg

---
+++ nabieva.liia/D2/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cstring>
+
+namespace nabieva
+{
+  struct PairCountSymbol
+  {
+    size_t count;
+    char symbol;
+  };
+  PairCountSymbol* addNewPair(const PairCountSymbol* a, size_t countPair, size_t num, char symb);
+  void printPairs(const PairCountSymbol* pairs, size_t countPair);
+}
+
+int main(int argc, char* argv[])
+{
+  if (argc > 1) {
+    std::cerr << "Invalid arguments\n";
+    return 1;
+  }
+  if (std::strcmp(argv[1], "reverse")) {
+    std::cerr << "Invalid arguments\n";
+    return 1;
+  }
+  char symb = ' ';
+  while (true) {
+    if (!(std::cin >> symb)) {
+      break;
+    }
+    if (std::cin.eof()) {
+      break;
+    }
+  }
+}
+
+nabieva::PairCountSymbol* nabieva::addNewPair(const nabieva::PairCountSymbol* a, size_t countPair, size_t num, char symb)
+{
+  nabieva::PairCountSymbol* aNew = new nabieva::PairCountSymbol[countPair + 1];
+  for (size_t i = 0; i < countPair; i++) {
+    aNew[i] = a[i];
+  }
+  aNew[countPair].count = num;
+  aNew[countPair].symbol = symb;
+  delete[] a;
+  return aNew;
+}
+void nabieva::printPairs(const nabieva::PairCountSymbol* pairs, size_t countPair)
+{
+  for (size_t i = 0; i < countPair; i++) {
+    std::cout << pairs[i].count << " " << pairs[i].symbol << "\n";
+  }
+}
