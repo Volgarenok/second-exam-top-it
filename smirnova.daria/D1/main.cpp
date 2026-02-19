6d873ce28ad7a3b5688f3421da73f296f1c26537
dassrm <daryasm1rnoff@yandex.ru>
2026-02-19 19:13:35 +0300

    fixed errors 2.0

--- smirnova.daria/D1/main.cpp
+++ smirnova.daria/D1/main.cpp
@@ -1,6 +1,7 @@
 #include "array_processing.hpp"
 
 #include <iostream>
+#include <utility>
 
 int main()
 {
@@ -9,13 +10,15 @@ int main()
 
   if (error_code == 1) {
     std::cerr << "Error: failed to read a pair element\n";
-    smirnova::printReverse(nullptr, 0);
+    smirnova::printReverse(result.first, result.second);
+    delete[] result.first;
     return 1;
   }
 
   if (error_code == 2) {
     std::cerr << "Error: failed to allocate memory\n";
-    smirnova::printReverse(nullptr, 0);
+    smirnova::printReverse(result.first, result.second);
+    delete[] result.first;
     return 2;
   }
 
