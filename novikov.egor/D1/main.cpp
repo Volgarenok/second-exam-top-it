f3cfee11128cd5e3dc4929d99a88372d64567f1b
EGGorkaa <egor_novikov20004@mail.ru>
2026-02-19 19:32:58 +0300

    fixed logic errors. W1 is done

--- novikov.egor/D1/main.cpp
+++ novikov.egor/D1/main.cpp
@@ -2,113 +2,145 @@
 
 namespace novikov
 {
-  char** extend(char** seqv, size_t s, size_t k)
-  {
-    char** new_seqv = new char*[k];
-    for(size_t i =0; i < s; ++i)
-    {
-      new_seqv[i] = seqv[i];
-    }
-    for(size_t i = s; i<k; ++i)
-    {
-      new_seqv[i] = nullptr;
-    }
-    delete[] seqv;
-    return new_seqv;
-  }
-  char** cut(char**seqv, size_t s)
-  {
-    char** new_seqv = new char*[s];
-    for(size_t i =0; i<s; ++i)
-    {
-      new_seqv[i] = seqv[i];
-    }
-    delete[] seqv;
-    return new_seqv;
-  }
-  int* extend(int* lens, size_t s, size_t k)
-  {
-    int* new_lens = new int[k];
-    for(size_t i =0; i<s; ++i)
-    {
-      new_lens[i] = lens[i];
-    }
-    for(size_t i = s; i<k; ++i)
-    {
-      new_lens[i] = 0;
-    }
-    delete[] lens;
-    return new_lens;
-  }
-  int* cut(int* lens, size_t s)
-  {
-    int* new_lens = new int[s];
-    for(size_t i =0; i<s; ++i)
-    {
-      new_lens[i] - lens[i];
-    }
-    delete[] lens;
-    return new_lens;
-  }
-
+  char** extend(char** seqv, size_t s, size_t k);
+  char** cut(char** seqv, size_t s);
+  int* extend(int* lens, size_t s, size_t k);
+  int* cut(int* lens, size_t s);
 }
 
 int main()
 {
-  int num = 0;
   size_t size = 0;
   char c = ' ';
-  char* seq;
   size_t size_seqv = 0;
   size_t cap_seqv = 3;
-  int* lens = new int[cap_seqv];
-  char** seqv = new char*[cap_seqv];
+  int* lens = nullptr;
+  char** seqv = nullptr;
   try
   {
-    while(std::cin)
-    { 
-      if(std::cin>>size)
+    lens = new int[cap_seqv];
+    seqv = new char*[cap_seqv];
+    
+    for(size_t i = 0; i < cap_seqv; ++i)
+    {
+      seqv[i] = nullptr;
+    }
+    
+    while(std::cin >> size >> c)
+    {
+      if(size == 0)
       {
-        lens[size_seqv] = size;
-        char* ceq = new char[size]; 
+        continue;
       }
-      else
+      
+      char* seq = new char[size];
+      
+      for(size_t i = 0; i < size; ++i)
       {
-        std::cerr<<"Invalid argument.\n";
-        return 1;
+        seq[i] = c;
       }
-      if(std::cin>>c)
-      {
-        for(size_t i = 0; i<size; ++i)
-        {
-          seq[i] = c;
-        }
-      }
-      else
-      {
-        std::cerr<<"Invalid argument.\n";
-        return 1;
-      }
+      
       if(size_seqv == cap_seqv)
       {
-        lens = novikov::extend(lens, cap_seqv, size_seqv+3);
-        seqv = novikov::extend(seqv, cap_seqv, size_seqv+3);
+        lens = novikov::extend(lens, cap_seqv, cap_seqv + 3);
+        seqv = novikov::extend(seqv, cap_seqv, cap_seqv + 3);
         cap_seqv += 3;
       }
-      seqv[cap_seqv] = seq;
-      cap_seqv++;
+      
+      lens[size_seqv] = size;
+      seqv[size_seqv] = seq;
+      size_seqv++;
     }
-  }
-  catch(std::bad_alloc)
-  {
-    std::cerr<<"Bad alloc.\n";
-    delete[] lens;
-    for(size_t i = 0; i<cap_seqv; ++i)
+    
+    if(size_seqv > 0)
+    {
+      for(size_t i = size_seqv; i > 0; --i)
+      {
+        size_t idx = i - 1;
+        for(int j = lens[idx] - 1; j >= 0; --j)
+        {
+          std::cout << seqv[idx][j];
+        }
+      }
+    }
+    
+    std::cout << std::endl;
+    
+    for(size_t i = 0; i < size_seqv; ++i)
     {
       delete[] seqv[i];
     }
     delete[] seqv;
+    delete[] lens;
+    
+    return 0;
+  }
+  catch(const std::bad_alloc&)
+  {
+    std::cerr << "Bad alloc.\n";
+    
+    if(seqv)
+    {
+      for(size_t i = 0; i < size_seqv; ++i)
+      {
+        delete[] seqv[i];
+      }
+      delete[] seqv;
+    }
+    
+    delete[] lens;
+    
     return 2;
   }
-  
+}
+
+char** novikov::extend(char** seqv, size_t s, size_t k)
+{
+  char** new_seqv = new char*[k];
+  for(size_t i =0; i < s; ++i)
+  {
+    new_seqv[i] = seqv[i];      
+  }
+  for(size_t i = s; i<k; ++i)
+  {
+    new_seqv[i] = nullptr;
+  }
+  delete[] seqv;
+  return new_seqv;
+}
+
+char** novikov::cut(char**seqv, size_t s)
+{
+  char** new_seqv = new char*[s];
+  for(size_t i =0; i<s; ++i)
+  {
+    new_seqv[i] = seqv[i];
+  }
+  delete[] seqv;
+  return new_seqv;
+}
+int* novikov::extend(int* lens, size_t s, size_t k)
+{
+  int* new_lens = new int[k];
+  for(size_t i =0; i<s; ++i)
+  {
+    new_lens[i] = lens[i];
+  }
+  for(size_t i = s; i<k; ++i)
+  {
+    new_lens[i] = 0;
+  }
+  delete[] lens;
+  return new_lens;
+}
+
+int* novikov::cut(int* lens, size_t s)
+{
+  int* new_lens = new int[s];
+  for(size_t i =0; i<s; ++i)
+  {
+    new_lens[i] = lens[i];
+  }
+  delete[] lens;
+  return new_lens;
 }
