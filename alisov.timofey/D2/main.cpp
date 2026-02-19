07d4a84253fe10ff1dd030db405e8fa89066ef6c
AlisovTimofey <timofeyalisov@gmail.com>
2026-02-19 19:31:05 +0300

    start working on main

--- alisov.timofey/D2/main.cpp
+++ alisov.timofey/D2/main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
 
-int main()
+int main(int argc, char *argv[])
 {
+  bool reverse = false;
+
+  if (argc > 2)
+  {
+    std::cerr << "Invalid arguments" << '\n';
+    return 1;
+  }
+
+  if (argc == 2)
+  {
+    if (std::string(argv[1]) == "reverse")
+      reverse = true;
+    else
+    {
+      std::cerr << "Invalid arguments" << '\n';
+      return 1;
+    }
+  }
 }
