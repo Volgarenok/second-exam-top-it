ee925463b2dc88073150fcf249ce6d68a7396995
blantality <parsoffgambit@gmail.com>
2026-02-19 18:52:34 +0300

    input

--- parsov.matvey/D1/main.cpp
+++ parsov.matvey/D1/main.cpp
@@ -1,4 +1,32 @@
+#include <iostream>
+
+namespace parsov
+{
+}
+
 int main()
 {
+  while (true)
+  {
+    unsigned int count = 0;
+    if (!(std::cin >> count))
+    {
+      if (std::cin.eof())
+      {
+        break;
+      }
+      std::cerr << "failed to read count\n";
+      return 1;
+    }
 
+    char symbol = '\0';
+    if (!(std::cin >> symbol))
+    {
+      std::cerr << "failed to read symbol\n";
+      return 1;
+    }
+  }
+
+  std::cout << "\n";
+  return 0;
 }
