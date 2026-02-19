1056c649600591d90a943b393187c83d3696228b
Владислав  Шигарев <vladislavsigarev@MBPWiFi.local>
2026-02-19 19:53:46 +0300

    fixed CG

--- shigarev.vladislav/D1/main.cpp
+++ shigarev.vladislav/D1/main.cpp
@@ -2,37 +2,47 @@
 #include <iostream>
 #include <new>
 
-namespace shigarev {
-  struct Pair {
-    unsigned int count;
-    char ch;
-};
+namespace shigarev
+{
+  struct pair_t
+  {
+    unsigned int count_;
+    char ch_;
+  };
 
-bool readPair(unsigned int & count, char & ch) {
-  return static_cast< bool >(std::cin >> count >> ch);
-}
+  bool readPair(unsigned int & count, char & ch)
+  {
+    return static_cast< bool >(std::cin >> count >> ch);
+  }
 
-class PairArray {
+  class PairArray
+  {
   public:
     PairArray():
       data_(nullptr),
       size_(0)
-    {}
+    {
+    }
 
     ~PairArray()
     {
       delete[] data_;
     }
 
-    size_t size() const
+    const pair_t & operator[](std::size_t i) const
+    {
+      return data_[i];
+    }
+
+    std::size_t getSize() const
     {
       return size_;
     }
 
-    void append(unsigned int count, char ch)
+    void append(const unsigned int count, const char ch)
     {
-      Pair * grown = new Pair[size_ + 1];
-      for (size_t i = 0; i < size_; ++i) {
+      pair_t * grown = new pair_t[size_ + 1];
+      for (std::size_t i = 0; i < size_; ++i) {
         grown[i] = data_[i];
       }
       grown[size_] = {count, ch};
@@ -41,34 +51,31 @@ class PairArray {
       ++size_;
     }
 
-    size_t computeLength() const {
-      size_t total = 0;
-      for (size_t i = 0; i < size_; ++i) {
-        total += data_[i].count;
+    std::size_t computeLength() const
+    {
+      std::size_t total = 0;
+      for (std::size_t i = 0; i < size_; ++i) {
+        total += data_[i].count_;
       }
       return total;
     }
 
-    const Pair & operator[](size_t i) const
-    {
-      return data_[i];
-    }
-
   private:
-    Pair * data_;
-    size_t size_;
-};
+    pair_t * data_;
+    std::size_t size_;
+  };
 
-class CharSequence {
+  class CharSequence
+  {
   public:
     explicit CharSequence(const PairArray & pairs):
       length_(pairs.computeLength()),
       data_(new char[length_])
     {
-      size_t pos = 0;
-      for (size_t i = 0; i < pairs.size(); ++i) {
-        for (unsigned int j = 0; j < pairs[i].count; ++j) {
-          data_[pos] = pairs[i].ch;
+      std::size_t pos = 0;
+      for (std::size_t i = 0; i < pairs.getSize(); ++i) {
+        for (unsigned int j = 0; j < pairs[i].count_; ++j) {
+          data_[pos] = pairs[i].ch_;
           ++pos;
         }
       }
@@ -78,30 +85,32 @@ class CharSequence {
     {
       delete[] data_;
     }
-  
-    size_t length() const
+
+    std::size_t getLength() const
     {
       return length_;
     }
 
     void printReverse() const
     {
-      for (size_t i = length_; i > 0; --i) {
+      for (std::size_t i = length_; i > 0; --i) {
         std::cout << data_[i - 1];
       }
       std::cout << '\n';
     }
 
   private:
-    size_t length_;
+    std::size_t length_;
     char * data_;
   };
 }
 
-int main() {
+int main()
+{
   shigarev::PairArray arr;
   unsigned int count = 0;
   char ch = '\0';
+
   while (std::cin.peek() != EOF) {
     if (!shigarev::readPair(count, ch)) {
       if (!std::cin.eof()) {
